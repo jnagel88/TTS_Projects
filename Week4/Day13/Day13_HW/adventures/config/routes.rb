213271b68aa9bd209ce0f2056f20d2f03a1f96cf
Joe Nagel <jnagel88@gmail.com>
2018-02-19 12:47:32 -0600

    Initial homework commit.

---
+++ Week4/Day13/Day13_HW/adventures/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'ilovetocode' => 'welcome#index'
+
+  resources :activities
+  resources :states
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
